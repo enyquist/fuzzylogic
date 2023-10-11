9114a612d8463a9e55a0c7ee4da792c061bca31c
enyquist <erik.nyquist@comcast.net>
2023-10-10 21:26:38 -0400

    Defuzzification

---
+++ fuzzylogic/defuzz/base.py
@@ -0,0 +1,21 @@
+# standard libraries
+from abc import ABC, abstractclassmethod
+
+# third party libraries
+import numpy as np
+
+# fuzzy logic libraries
+from fuzzylogic.mf.base import MembershipFunction1D
+
+
+class Defuzzification(ABC):
+    """
+    Fuzzy Inference Engine base class.
+    """
+
+    @abstractclassmethod
+    def defuzz(cls, x: np.ndarray, mf: MembershipFunction1D) -> float:
+        """
+        Defuzzifies the membership function.
+        """
+        pass
