5a9b6a5110d009d623e8c504bb303cd0190c49d4
enyquist <erik.nyquist@comcast.net>
2023-10-13 10:06:51 -0400

    Fuzzy Inference Engines and Rules

---
+++ fuzzylogic/engines/fuzzy_engine.py
@@ -0,0 +1,148 @@
+# standard libraries
+from functools import reduce
+from typing import List, Union
+
+# third party libraries
+import numpy as np
+
+# fuzzy logic libraries
+from fuzzylogic.defuzz.defuzz import Bisector, Centroid, LargestOfMaximum, MeanOfMaximum, SmallestOfMaximum
+from fuzzylogic.mf.base import MembershipFunction1D
+from fuzzylogic.rules.fuzzy_rule import FuzzyRule
+from fuzzylogic.tconorms.tconorms import TCoNorm
+
+DEFUZZ = {
+    "centroid": Centroid,
+    "bisector": Bisector,
+    "mom": MeanOfMaximum,
+    "lom": LargestOfMaximum,
+    "som": SmallestOfMaximum,
+}
+
+
+class FuzzyEngine:
+    """
+    Base class for fuzzy inference engines.
+    """
+
+    def __init__(
+        self,
+        rules: List[FuzzyRule],
+        aggregate_operator: TCoNorm,
+        defuzz: str = "centroid",
+    ):
+        """
+        Initialises a fuzzy inference engine.
+
+        Args:
+            rules (List[FuzzyRule]): List of fuzzy rules
+            aggregate_operator (TCoNorm): Operator to aggregate fuzzy rules
+            defuzz (Defuzzification): Defuzzification method
+        """
+        # Check if the rules are valid
+        if not rules:
+            raise ValueError("The rules list cannot be empty.")
+
+        # Check if the aggregate operator is valid
+        if not isinstance(aggregate_operator, TCoNorm):
+            raise TypeError(f"The aggregate operator must be a TCoNorm. Got {type(aggregate_operator)}.")
+
+        # Check if the defuzzification method is valid
+        if defuzz not in DEFUZZ:
+            raise ValueError(f"The defuzzification method is not valid. Got {defuzz}.")
+
+        self.rules = rules
+        self.aggregate_operator = aggregate_operator
+        self.defuzz = DEFUZZ[defuzz]
+
+    def __repr__(self) -> str:
+        """
+        Returns the string representation of the fuzzy inference engine.
+        """
+        rules = self.rules
+        op = self.aggregate_operator
+        defuzz = self.defuzz.__name__
+
+        return f"FuzzyEngine(rules={rules}, aggregate_operator={op}, defuzz={defuzz})"
+
+    def compose(self, inputs: Union[float, List[float]]) -> MembershipFunction1D:
+        """
+        Composes the fuzzy inference engine.
+
+        Args:
+            x (Union[float, List[float]]): Crips input values
+        """
+
+        # Evaluate the rules
+        clipped_consequent_mfs = [rule.evaluate(inputs) for rule in self.rules]
+
+        # Aggregate the rules
+        self.composed_consequent_mf = reduce(self.aggregate_operator.combine, clipped_consequent_mfs)
+
+        return self.composed_consequent_mf
+
+    def infer(self, x: np.ndarray) -> np.ndarray:
+        """
+        Performs fuzzy inference.
+
+        Args:
+            x (np.ndarray): Universe of discourse
+
+        Returns:
+            np.ndarray: Membership values of the output variable
+        """
+
+        # Check if the composed consequent membership function exists
+        if not hasattr(self, "composed_consequent_mf"):
+            raise ValueError("The fuzzy inference engine has not been composed yet. Call the compose method first.")
+
+        # Defuzzify the composed consequent membership function
+        defuzzified_value = self.defuzz.defuzz(x, self.composed_consequent_mf)
+
+        return defuzzified_value
+
+    def calculate_fuzzy_control_surface(self, antecedent_ranges: List[np.ndarray]) -> np.ndarray:
+        """
+        Calculates the fuzzy control surface of the fuzzy inference engine.
+
+        Args:
+            antecedent_ranges (List[np.ndarray]): List of numpy arrays containing the antecedent ranges
+
+        Returns:
+            np.ndarray: Fuzzy control surface
+        """
+
+        # Create a multi-dimensional meshgrid from the antecedent ranges
+        meshgrid = np.meshgrid(*antecedent_ranges)
+
+        # Flatten the meshgrid for easier iteration
+        flat_meshgrid = [grid.flatten() for grid in meshgrid]
+
+        # Calculate the total number of points
+        total_points = len(flat_meshgrid[0])
+
+        # Initialize the fuzzy surface
+        outputs = np.zeros(total_points)
+
+        # Iterate over the flattened meshgrid
+        for idx, point in enumerate(zip(*flat_meshgrid)):
+            # Create a list of coordinates for this point
+            coords = list(point)
+
+            # Compose the engine with the current inputs
+            self.compose(coords)
+
+            # Get Rule Strength (Degree of Membership) from the rules
+            rule_doms = [np.minimum(*rule.dom) for rule in self.rules]
+
+            # Aggregate the rule strengths
+            aggregated_rule_dom = np.maximum(*rule_doms) if len(self.rules) > 1 else rule_doms[0]
+
+            # Calculate the output value
+            outputs[idx] = self.defuzz.defuzz(aggregated_rule_dom, self.composed_consequent_mf)
+
+        # Reshape the fuzzy surface
+        output_shape = tuple(len(range_) for range_ in antecedent_ranges)
+        outputs = outputs.reshape(output_shape)
+
+        return outputs
---
+++ fuzzylogic/rules/fuzzy_rule.py
@@ -0,0 +1,194 @@
+# standard libraries
+from typing import List, Optional, Union
+
+# fuzzy logic libraries
+from fuzzylogic.mf.base import MembershipFunction1D
+from fuzzylogic.mf.constant import ConstantMF
+from fuzzylogic.mf.singleton import FuzzySingleton
+from fuzzylogic.tconorms.base import TCoNorm
+from fuzzylogic.tnorms.base import TNorm
+
+
+class FuzzyRule:
+    """
+    Fuzzy rule class
+    """
+
+    def __init__(
+        self,
+        antecedents: List[MembershipFunction1D],
+        operators: List[str],
+        consequent: MembershipFunction1D,
+        dom_operator: TNorm,
+        tnorm: TNorm,
+        tconorm: TCoNorm,
+        implication_operator: Union[TNorm, TCoNorm],
+        mf_names: Optional[List[str]] = None,
+    ):
+        """
+        Args:
+            antecedents (List[MembershipFunction1D]): antecedents of the rule
+            operators (List[str]): operators to combine the antecedents
+            consequent (MembershipFunction1D): consequent of the rule
+            dom_operator (TNorm): operator to calculate the degree of membership of the antecedents
+            tnorm (TNorm): operator to calculate the degree of membership of the consequent aka the AND operator
+            tconorm (TCoNorm): operator to calculate the degree of membership of the consequent aka the OR operator
+            implication_operator (Union[TNorm, TCoNorm]): operator to calculate the qualified consequent
+            mf_names (Optional[List[str]], optional): names of the antecedents and consequent. Defaults to None.
+        """
+
+        # Check if antecedents are valid
+        if not isinstance(antecedents, list):
+            raise TypeError(f"Expected antecedents to be a list, but got {type(antecedents)}")
+        if not all(isinstance(antecedent, MembershipFunction1D) for antecedent in antecedents):
+            raise TypeError(
+                f"Expected antecedents to be a list of MembershipFunction1D, but got {type(antecedents[0])}"
+            )
+
+        # Check if operators are valid
+        if len(operators) != len(antecedents) - 1:
+            raise ValueError(f"Expected operators to have length {len(antecedents) - 1}, but got {len(operators)}")
+
+        # Check if consequent is valid
+        if not isinstance(consequent, MembershipFunction1D):
+            raise TypeError(f"Expected consequent to be a MembershipFunction1D, but got {type(consequent)}")
+
+        # Check if Degree of Membership operator is valid
+        if not isinstance(dom_operator, TNorm):
+            raise TypeError(f"Expected dom_operator to be a TNorm, but got {type(dom_operator)}")
+
+        # Check if TNorm operator is valid
+        if not isinstance(tnorm, TNorm):
+            raise TypeError(f"Expected tnorm to be a TNorm, but got {type(tnorm)}")
+
+        # Check if TConorm operator is valid
+        if not isinstance(tconorm, TCoNorm):
+            raise TypeError(f"Expected tconorm to be a TCoNorm, but got {type(tconorm)}")
+
+        # Check if operator is valid
+        if not isinstance(implication_operator, TNorm) and not isinstance(implication_operator, TCoNorm):
+            raise TypeError(f"Expected operator to be a TNorm or TConorm, but got {type(implication_operator)}")
+
+        # Check if mf_names is valid
+        if mf_names is not None:
+            if not isinstance(mf_names, list):
+                raise TypeError(f"Expected mf_names to be a list, but got {type(mf_names)}")
+            if not all(isinstance(mf_name, str) for mf_name in mf_names):
+                raise TypeError(f"Expected mf_names to be a list of strings, but got {type(mf_names[0])}")
+            if len(mf_names) != len(antecedents) + 1:
+                raise ValueError(f"Expected mf_names to have length {len(antecedents) + 1}, but got {len(mf_names)}")
+
+        self.antecedents = antecedents
+        self.operators = operators
+        self.consequent = consequent
+        self.dom_operator = dom_operator
+        self.tnorm_operator = tnorm
+        self.tconorm_operator = tconorm
+        self.implication_operator = implication_operator
+        self.mf_names = mf_names
+
+    def __repr__(self):
+        # Handling the case when there are no antecedents or just one
+        if not self.antecedents:
+            return "FuzzyRule()"
+        elif len(self.antecedents) == 1:
+            single_antecedent = self.mf_names[0] if self.mf_names else repr(self.antecedents[0])
+            return f"FuzzyRule({single_antecedent}, {self.consequent!r})"
+
+        # Constructing the representation string with mf_names if available
+        repr_str = "IF "
+        for i, antecedent in enumerate(self.antecedents):
+            antecedent_repr = self.mf_names[i] if self.mf_names else repr(antecedent)
+            repr_str += f"{antecedent_repr} "
+            if i < len(self.operators):
+                repr_str += f"{self.operators[i].upper()} "
+
+        consequent_repr = self.mf_names[-1] if self.mf_names else repr(self.consequent)
+        repr_str += f"THEN {consequent_repr}"
+
+        return repr_str
+
+    def evaluate(self, x: Union[float, List[float]]) -> MembershipFunction1D:
+        """
+        Evaluate the rule.
+
+        Args:
+            x (Union[float, List[float]]): Crisp input
+
+        Returns:
+            MembershipFunction1D: Clipped consequent
+        """
+
+        # Get the degree of membership of the antecedents
+        self.dom = self.get_rule_strength(x)
+
+        # Cast antecedent_dom to MembershipFunction1D
+        antecedent_dom_mfs = [ConstantMF(value=antecedent_dom_i) for antecedent_dom_i in self.dom]
+
+        # Rule firing strength - using reduce to successively combine all membership functions
+        rule_firing_strength_mf = self._calculate_rule_firing_strength(antecedent_dom_mfs)
+
+        # Clip consequent using implication operator
+        clipped_consequent_mf = self.implication_operator.combine(rule_firing_strength_mf, self.consequent)
+
+        return clipped_consequent_mf
+
+    def _calculate_rule_firing_strength(self, antecedent_dom_mfs: List[MembershipFunction1D]) -> MembershipFunction1D:
+        """
+        Calculate the rule firing strength.
+
+        Args:
+            antecedent_dom_mfs (List[MembershipFunction1D]): List of antecedent degree of membership
+                membership functions
+
+        Returns:
+            MembershipFunction1D: Combined Antecedent Membership Function
+        """
+
+        # start with first antecedent
+        rule_firing_strength_mf = antecedent_dom_mfs[0]
+
+        # combine all antecedents
+        for idx, operator in enumerate(self.operators):
+            if operator == "and":
+                rule_firing_strength_mf = self.tnorm_operator.combine(
+                    rule_firing_strength_mf, antecedent_dom_mfs[idx + 1]
+                )
+            elif operator == "or":
+                rule_firing_strength_mf = self.tconorm_operator.combine(
+                    rule_firing_strength_mf, antecedent_dom_mfs[idx + 1]
+                )
+            else:
+                raise ValueError(f"Expected operator to be 'and' or 'or', but got {operator} at index {idx}")
+
+        return rule_firing_strength_mf
+
+    def get_rule_strength(self, x: Union[float, List[float]]) -> MembershipFunction1D:
+        """
+        Get the rule strength aka the degree of membership of the antecedents.
+
+        Args:
+            x (Union[float, List[float]]): Crisp input
+
+        Returns:
+            MembershipFunction1D: Clipped consequent
+        """
+
+        # Cast x to list if it is a float
+        if isinstance(x, float):
+            x = [x]
+
+        # Assert length of x is equal to the number of antecedents
+        if len(x) != len(self.antecedents):
+            raise ValueError(f"Expected x to have length {len(self.antecedents)}, but got {len(x)}")
+
+        # Fuzzify Crisp input
+        fuzzy_x = [FuzzySingleton(value=x_i) for x_i in x]
+
+        # Degree of membership of antecedents
+        antecedent_dom = [
+            self.dom_operator.combine(fuzzy_x_i, antecedent)(x_i)
+            for fuzzy_x_i, antecedent, x_i in zip(fuzzy_x, self.antecedents, x)
+        ]
+
+        return antecedent_dom
